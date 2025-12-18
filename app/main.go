37a51a598d00bfa2487d6ebf65e519dfc025c666
Nermin Sehic <sehicnermin97@gmail.com>
2025-12-18 20:33:37 +1100

    Implement exit

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" and "os" imports in stage 1 (feel free to remove this!)
@@ -13,9 +14,14 @@ var _ = os.Stdout
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Fprint(os.Stdout, "$ ")
+repl:
 	for scanner.Scan() {
-		line := scanner.Text()
-		fmt.Printf("%s: command not found\n", line)
+		command := strings.TrimSpace(scanner.Text())
+		switch command {
+		case "exit":
+			break repl
+		}
+		fmt.Printf("%s: command not found\n", command)
 		fmt.Fprint(os.Stdout, "$ ")
 	}
 }
