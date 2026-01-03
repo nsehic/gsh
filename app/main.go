4fbbc336abeb5ded056e289a027bb323bc4977f0
Nermin Sehic <sehicnermin97@gmail.com>
2026-01-03 22:32:50 +1100

    refactor parser

--- app/main.go
+++ app/main.go
@@ -6,70 +6,18 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"strings"
 
 	"github.com/codecrafters-io/shell-starter-go/commands"
 )
 
-func getNextChar(idx int, str string) string {
-	r := []rune(str)
-	if idx >= len(str)-1 {
-		return ""
-	}
-	return string(r[idx+1])
-}
-
-func parseInput(input string) (string, []string) {
-	line := []string{}
-	singleQuote := false
-	concatString := false
-	var sb strings.Builder
-	for i, c := range input {
-		switch string(c) {
-		case "'":
-			if singleQuote {
-				if concatString {
-					concatString = false
-					continue
-				} else if getNextChar(i, input) != " " {
-					concatString = true
-				} else {
-					singleQuote = false
-					if sb.Len() > 0 {
-						line = append(line, sb.String())
-						sb.Reset()
-					}
-				}
-			} else {
-				singleQuote = true
-			}
-		case " ":
-			if singleQuote {
-				sb.WriteRune(c)
-			} else if sb.Len() > 0 {
-				line = append(line, sb.String())
-				sb.Reset()
-			}
-		default:
-			sb.WriteRune(c)
-		}
-	}
-
-	if sb.Len() > 0 {
-		line = append(line, sb.String())
-		sb.Reset()
-	}
-
-	return line[0], line[1:]
-}
-
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+	var parser Parser
 
 	for {
 		fmt.Print("$ ")
 		if scanner.Scan() {
-			command, args := parseInput(scanner.Text())
+			command, args := parser.Parse(scanner.Text())
 
 			err := commands.ExecuteBuiltin(command, args)
 			if errors.Is(err, commands.ErrBuiltinNotExists) {
---
+++ app/parser.go
@@ -0,0 +1,66 @@
+package main
+
+import "strings"
+
+type Parser struct {
+	result       []string
+	singleQuote  bool
+	concatString bool
+	stringBuffer strings.Builder
+	input        string
+}
+
+func (p *Parser) Parse(input string) (string, []string) {
+	p.input = input
+	defer p.Reset()
+	for i, c := range input {
+		switch string(c) {
+		case "'":
+			if p.singleQuote {
+				if p.concatString {
+					p.concatString = false
+				} else if p.getNextChar(i) != " " {
+					p.concatString = true
+				} else {
+					p.singleQuote = false
+					p.flushStringBuffer()
+				}
+			} else {
+				p.singleQuote = true
+			}
+		case " ":
+			if p.singleQuote {
+				p.stringBuffer.WriteRune(c)
+			} else {
+				p.flushStringBuffer()
+			}
+		default:
+			p.stringBuffer.WriteRune(c)
+		}
+	}
+	p.flushStringBuffer()
+	return p.result[0], p.result[1:]
+}
+
+func (p *Parser) getNextChar(pos int) string {
+	r := []rune(p.input)
+	if pos >= len(r)-1 {
+		return ""
+	}
+	return string(r[pos+1])
+}
+
+func (p *Parser) Reset() {
+	p.input = ""
+	p.result = []string{}
+	p.stringBuffer.Reset()
+	p.singleQuote = false
+	p.concatString = false
+}
+
+func (p *Parser) flushStringBuffer() {
+	if p.stringBuffer.Len() > 0 {
+		p.result = append(p.result, p.stringBuffer.String())
+		p.stringBuffer.Reset()
+	}
+}
