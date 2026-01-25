3224006189350e9ae458d486dbe963c6b9c52304
Nermin Sehic <sehicnermin97@gmail.com>
2026-01-25 16:03:12 +1100

    fix: Builtin completion

--- app/main.go
+++ app/main.go
@@ -1,31 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
 	"os"
 	"os/exec"
-	"os/signal"
 	"path/filepath"
 	"strings"
-	"syscall"
 
 	"github.com/codecrafters-io/shell-starter-go/autocomplete"
 	"github.com/codecrafters-io/shell-starter-go/commands"
 	"golang.org/x/term"
 )
-
-const prompt = "$ "
-
-func redrawLine(line []rune) {
-	fmt.Fprint(os.Stdout, "\r\033[K")
-	fmt.Fprint(os.Stdout, prompt)
-	fmt.Fprint(os.Stdout, string(line))
-}
-
-func printPrompt() {
-	fmt.Fprintf(os.Stdout, "\r%s", prompt)
-}
 
 func getOutputFile(path string, append bool) (*os.File, bool) {
 	flag := os.O_CREATE | os.O_WRONLY
@@ -65,109 +52,111 @@ func getStderrFile(path string, append bool) (*os.File, bool) {
 	return getOutputFile(path, append)
 }
 
-func main() {
+type LineReader struct {
+	prompt         string
+	autocompletion *autocomplete.Trie
+}
+
+func (lr *LineReader) RedrawLine(line string) {
+	fmt.Fprint(os.Stdout, "\r\033[K")
+	fmt.Fprint(os.Stdout, lr.prompt)
+	fmt.Fprint(os.Stdout, line)
+}
+
+func (lr *LineReader) ReadLine() (string, error) {
 	fd := int(os.Stdin.Fd())
 	if !term.IsTerminal(fd) {
 		fmt.Fprintf(os.Stderr, "error: stdin is not a terminal\n")
-		return
+		return "", errors.New("stdin is not a terminal")
 	}
 
 	oldState, err := term.MakeRaw(fd)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v", err)
-		return
+		return "", err
 	}
 	defer term.Restore(fd, oldState)
 
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+	var line strings.Builder
+	reader := bufio.NewReader(os.Stdin)
 
-	go func() {
-		<-sig
-		term.Restore(fd, oldState)
-		os.Exit(1)
-	}()
-
-	trie := autocomplete.NewTrie("echo", "exit")
-
-	buf := make([]byte, 1)
-	line := []rune{}
-	printPrompt()
-	var parser Parser
-
+	fmt.Fprintf(os.Stdout, "\r%s", lr.prompt)
 	for {
-		_, err := os.Stdin.Read(buf)
+		b, err := reader.ReadByte()
 		if err != nil {
-			break
+			return line.String(), err
 		}
-		b := buf[0]
-
 		switch b {
 		case '\r', '\n':
 			os.Stdout.Write([]byte("\r\n"))
-
-			input := string(line)
-			line = nil // reset buffer
-
-			if strings.TrimSpace(input) == "" {
-				printPrompt()
-				break
-			}
-
-			parseResult := parser.Parse(input)
-			if parseResult != nil {
-				stdout, shouldCloseStdout := getStdoutFile(
-					parseResult.StdoutPath,
-					parseResult.StdoutAppend,
-				)
-				stderr, shouldCloseStderr := getStderrFile(
-					parseResult.StderrPath,
-					parseResult.StderrAppend,
-				)
-
-				cmd := commands.Command(parseResult.Command, parseResult.Args...)
-				cmd.Out = stdout
-				cmd.Err = stderr
-
-				if err := cmd.Run(); err != nil {
-					var execError *exec.Error
-					var pathError *os.PathError
-
-					switch {
-					case errors.As(err, &execError):
-						fmt.Fprintf(cmd.Err, "%s: not found\n", cmd.Name)
-					case errors.As(err, &pathError):
-						fmt.Fprintf(cmd.Err, "%s: %v\n", cmd.Name, pathError.Err)
-					}
-				}
-
-				if shouldCloseStdout {
-					stdout.Close()
-				}
-				if shouldCloseStderr {
-					stderr.Close()
-				}
-			}
-			printPrompt()
-
+			return line.String(), nil
 		case '\t':
-			// Autocomplete
-			completions := trie.Complete(string(line))
+			completions := lr.autocompletion.Complete(line.String())
 			if len(completions) == 0 {
 				break
 			}
-			line = []rune(fmt.Sprintf("%s ", completions[0]))
-			redrawLine(line)
-		case 127:
-			// Backspace
-			if len(line) > 0 {
-				line = line[:len(line)-1]
+			line.Reset()
+			line.WriteString(fmt.Sprintf("%s ", completions[0]))
+			lr.RedrawLine(line.String())
+		case 127, 8:
+			if line.Len() > 0 {
+				s := line.String()
+				line.Reset()
+				line.WriteString(s[:len(s)-1])
 				os.Stdout.Write([]byte("\b \b"))
 			}
 		default:
-			// Normal printable character
-			line = append(line, rune(b))
+			line.WriteByte(b)
 			os.Stdout.Write([]byte{b})
+		}
+	}
+}
+
+func main() {
+	lr := LineReader{
+		autocompletion: autocomplete.NewTrie("echo", "exit"),
+		prompt:         "$ ",
+	}
+
+	var parser Parser
+
+	for {
+		input, err := lr.ReadLine()
+		if err != nil {
+			fmt.Printf("error: %v\n", err)
+		}
+		if parseResult := parser.Parse(input); parseResult != nil {
+			stdout, shouldCloseStdout := getStdoutFile(
+				parseResult.StdoutPath,
+				parseResult.StdoutAppend,
+			)
+			stderr, shouldCloseStderr := getStderrFile(
+				parseResult.StderrPath,
+				parseResult.StderrAppend,
+			)
+
+			cmd := commands.Command(parseResult.Command, parseResult.Args...)
+			cmd.Out = stdout
+			cmd.Err = stderr
+
+			if err := cmd.Run(); err != nil {
+				var execError *exec.Error
+				var pathError *os.PathError
+
+				switch {
+				case errors.As(err, &execError):
+					fmt.Fprintf(cmd.Err, "%s: not found\n", cmd.Name)
+				case errors.As(err, &pathError):
+					fmt.Fprintf(cmd.Err, "%s: %v\n", cmd.Name, pathError.Err)
+				}
+			}
+
+			if shouldCloseStdout {
+				stdout.Close()
+			}
+			if shouldCloseStderr {
+				stderr.Close()
+			}
 		}
 	}
 }
